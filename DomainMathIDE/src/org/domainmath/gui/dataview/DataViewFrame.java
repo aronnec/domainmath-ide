f9dcb25e88bf027e319f9a73f2e8b144bc1f2b16
vinukn <vinuknmgc@gmail.com>
2013-01-09 10:57:59 +0530

    Improvement of DataViewFrame part-2

--- DomainMathIDE/src/org/domainmath/gui/dataview/DataViewFrame.java
+++ DomainMathIDE/src/org/domainmath/gui/dataview/DataViewFrame.java
@@ -20,11 +20,15 @@ package org.domainmath.gui.dataview;
 import java.awt.Desktop;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.print.PrinterException;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 import org.domainmath.gui.about.AboutDlg;
 
 
@@ -33,6 +37,130 @@ public class DataViewFrame extends javax.swing.JFrame {
     private java.net.URL imgURL = getClass().getResource("Resources/DomainMath.png");
     private Image icon = Toolkit.getDefaultToolkit().getImage(imgURL);
     private final DataViewPanel newContentPane;
+    
+    public static String REARRANGE_MATRIX_FUNCTIONS[] = {   "fliplr",
+                                                            "flipud",
+                                                            "flipdim",
+                                                            "rot90",
+                                                            "rotdim",
+                                                            "sort", 
+                                                            "sortrows",
+                                                            "tril",
+                                                            "vec",
+                                                            "vech",
+                                                            "diag",
+                                                            "inverse",
+                                                            "transpose"};
+    
+    
+    public static String EXPONENTS_AND_LOGARITHMS_FUNCTIONS[] = {   "exp",
+                                                                    "expm1", 
+                                                                    "log",
+                                                                    "reallog",
+                                                                    "log1p",
+                                                                    "log10", 
+                                                                    "log2",
+                                                                    "pow2",
+                                                                    "nextpow2",
+                                                                    "realpow",
+                                                                    "sqrt",
+                                                                    "realsqrt",
+                                                                    "cbrt"};
+    
+    public static String COMPLEX_FUNCTIONS[] = {    "abs",
+                                                    "arg",
+                                                    "conj",
+                                                    "cplxpair",
+                                                    "imag",
+                                                    "real"}; 
+    
+    public static String TRIGONOMETRY_FUNCTIONS[] = {   "sin",
+                                                        "cos",
+                                                        "tan",
+                                                        "sec",
+                                                        "csc",
+                                                        "cot",
+                                                        "asin",
+                                                        "acos",
+                                                        "atan",
+                                                        "asec",
+                                                        "acsc",
+                                                        "acot",
+                                                        "sinh",
+                                                        "cosh",
+                                                        "tanh",
+                                                        "sech",
+                                                        "csch",
+                                                        "coth"};
+    public static String TRIGONOMETRY_INVERSE_FUNCTIONS[] = {  "asin",
+                                                        "acos",
+                                                        "atan",
+                                                        "asec",
+                                                        "acsc",
+                                                        "acot",
+                                                        "asinh",
+                                                        "acosh",
+                                                        "atanh",
+                                                        "asech",
+                                                        "acsch",
+                                                        "acoth",
+                                                        "asind",
+                                                        "acosd",
+                                                        "atand",
+                                                        "asecd",
+                                                        "acscd",
+                                                        "acotd"};
+    public static String SUMS_AND_PRODUCTS_FUNCTIONS[] = {      "sum",
+                                                                "prod",
+                                                                "cumsum",
+                                                                "cumprod"};
+    public static String SPECIAL_FUNCTIONS[] = {    "erf",
+                                                "erfc",
+                                                "erfcx", 
+                                                "erfinv",
+                                                "gamma",
+                                                "lgamma"};
+    
+    public static String UTILITY_FUNCTIONS[] = {"ceil",
+                                                "fix",
+                                                "floor",
+                                                "round",
+                                                "roundb",
+                                                "max",
+                                                "min",
+                                                "cummax",
+                                                "cummin",
+                                                "gradient",
+                                                "del2",
+                                                "factorial",
+                                                "factor",
+                                                "primes",
+                                                "sign"};
+    
+    public static String STATISTICS_FUNCTIONS[] ={  "mean", 
+                                                    "median",
+                                                    "mode",
+                                                    "range",
+                                                    "iqr",
+                                                    "meansq", 
+                                                    "std",
+                                                    "var",
+                                                    "skewness",
+                                                    "kurtosis",
+                                                    "statistics",
+                                                    "center",
+                                                    "zscore",
+                                                    "perms",
+                                                    "probit", 
+                                                    "cloglog",
+                                                    "table", 
+                                                    "cov",
+                                                    "corr",
+                                                    "spearman",
+                                                    "kendall"};
+    
+    private  JMenuItem functionsItem;
+    private final JMenu trigonometryInverseMenu;
     public DataViewFrame(String path) {
          
          File f = new File(path);
@@ -43,6 +171,59 @@ public class DataViewFrame extends javax.swing.JFrame {
                     setContentPane(newContentPane);
                     pack();
             initComponents();
+            
+            for(int i= 0; i<REARRANGE_MATRIX_FUNCTIONS.length; i++) {
+               functionsItem =new JMenuItem(REARRANGE_MATRIX_FUNCTIONS[i].toUpperCase()) ;
+               this.rearrageMtxMenu.add(functionsItem);
+            }
+            
+             for(int i= 0; i<EXPONENTS_AND_LOGARITHMS_FUNCTIONS.length; i++) {
+                functionsItem=new JMenuItem(EXPONENTS_AND_LOGARITHMS_FUNCTIONS[i].toUpperCase()) ;
+               this.expAndLogMenu.add(functionsItem);
+            }
+             
+            for(int i= 0; i<COMPLEX_FUNCTIONS.length; i++) {
+                functionsItem=new JMenuItem(COMPLEX_FUNCTIONS[i].toUpperCase()) ;
+               this.complexMenu.add(functionsItem);
+            }
+            
+            for(int i= 0; i<TRIGONOMETRY_FUNCTIONS.length; i++) {
+               functionsItem =new JMenuItem(TRIGONOMETRY_FUNCTIONS[i].toUpperCase()) ;
+               this.trigonometryMenu.add(functionsItem);
+            }
+            trigonometryInverseMenu = new JMenu("Inverse Functions");
+            this.trigonometryMenu.add(trigonometryInverseMenu);
+            
+            for(int i= 0; i<TRIGONOMETRY_INVERSE_FUNCTIONS.length; i++) {
+               functionsItem =new JMenuItem(TRIGONOMETRY_INVERSE_FUNCTIONS[i].toUpperCase()) ;
+               this.trigonometryInverseMenu.add(functionsItem);
+            }
+             for(int i= 0; i< SUMS_AND_PRODUCTS_FUNCTIONS.length; i++) {
+               functionsItem =new JMenuItem( SUMS_AND_PRODUCTS_FUNCTIONS[i].toUpperCase()) ;
+               this.sumProdMenu.add(functionsItem);
+            }
+              for(int i= 0; i<UTILITY_FUNCTIONS.length; i++) {
+               functionsItem =new JMenuItem(UTILITY_FUNCTIONS[i].toUpperCase()) ;
+               this.utilityMenu.add(functionsItem);
+            }
+             
+           for(int i= 0; i<SPECIAL_FUNCTIONS.length; i++) {
+               functionsItem =new JMenuItem(SPECIAL_FUNCTIONS[i].toUpperCase()) ;
+               this.specialFunctionsMenu.add(functionsItem);
+            }
+           for(int i= 0; i<STATISTICS_FUNCTIONS.length; i++) {
+               functionsItem =new JMenuItem(STATISTICS_FUNCTIONS[i].toUpperCase()) ;
+               this.StatisticsMenu.add(functionsItem);
+            }
+        
+           functionsItem.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               
+            }
+
+           });
         this.setIconImage(icon);
         setLocationRelativeTo(null);
         setVisible(true);
@@ -65,7 +246,18 @@ public class DataViewFrame extends javax.swing.JFrame {
         refreshItem = new javax.swing.JMenuItem();
         printItem = new javax.swing.JMenuItem();
         exitItem = new javax.swing.JMenuItem();
-        matrixMenu = new javax.swing.JMenu();
+        formulasMenu = new javax.swing.JMenu();
+        rearrageMtxMenu = new javax.swing.JMenu();
+        jSeparator1 = new javax.swing.JPopupMenu.Separator();
+        expAndLogMenu = new javax.swing.JMenu();
+        complexMenu = new javax.swing.JMenu();
+        trigonometryMenu = new javax.swing.JMenu();
+        jSeparator2 = new javax.swing.JPopupMenu.Separator();
+        sumProdMenu = new javax.swing.JMenu();
+        utilityMenu = new javax.swing.JMenu();
+        specialFunctionsMenu = new javax.swing.JMenu();
+        jSeparator3 = new javax.swing.JPopupMenu.Separator();
+        StatisticsMenu = new javax.swing.JMenu();
         helpMenu2 = new javax.swing.JMenu();
         forumItem2 = new javax.swing.JMenuItem();
         onlineHelpItem = new javax.swing.JMenuItem();
@@ -115,8 +307,37 @@ public class DataViewFrame extends javax.swing.JFrame {
 
         jMenuBar1.add(fileMenu);
 
-        matrixMenu.setText(bundle.getString("matrixMenu.title")); // NOI18N
-        jMenuBar1.add(matrixMenu);
+        formulasMenu.setText(bundle.getString("formulasMenu.title")); // NOI18N
+
+        rearrageMtxMenu.setText(bundle.getString("rearrageMtxMenu.title")); // NOI18N
+        formulasMenu.add(rearrageMtxMenu);
+        formulasMenu.add(jSeparator1);
+
+        expAndLogMenu.setText(bundle.getString("expAndLogMenu.title")); // NOI18N
+        formulasMenu.add(expAndLogMenu);
+
+        complexMenu.setText(bundle.getString("complexMenu.title")); // NOI18N
+        complexMenu.setActionCommand("");
+        formulasMenu.add(complexMenu);
+
+        trigonometryMenu.setText(bundle.getString("trigonometryMenu.title")); // NOI18N
+        formulasMenu.add(trigonometryMenu);
+        formulasMenu.add(jSeparator2);
+
+        sumProdMenu.setText(bundle.getString("sumProdMenu.title")); // NOI18N
+        formulasMenu.add(sumProdMenu);
+
+        utilityMenu.setText(bundle.getString("utilityMenu.title")); // NOI18N
+        formulasMenu.add(utilityMenu);
+
+        specialFunctionsMenu.setText(bundle.getString("specialFunctionsMenu.title")); // NOI18N
+        formulasMenu.add(specialFunctionsMenu);
+        formulasMenu.add(jSeparator3);
+
+        StatisticsMenu.setText(bundle.getString("StatisticsMenu.title")); // NOI18N
+        formulasMenu.add(StatisticsMenu);
+
+        jMenuBar1.add(formulasMenu);
 
         java.util.ResourceBundle bundle1 = java.util.ResourceBundle.getBundle("org/domainmath/gui/resources/DomainMath_en"); // NOI18N
         helpMenu2.setText(bundle1.getString("helpMenu.name")); // NOI18N
@@ -291,26 +512,33 @@ public class DataViewFrame extends javax.swing.JFrame {
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenuItem AboutItem;
+    private javax.swing.JMenu StatisticsMenu;
+    private javax.swing.JMenu complexMenu;
     private javax.swing.JMenuItem exitItem;
+    private javax.swing.JMenu expAndLogMenu;
     private javax.swing.JMenuItem faqItem;
     private javax.swing.JMenuItem feedBackItem1;
     private javax.swing.JMenu fileMenu;
-    private javax.swing.JMenuItem forumItem;
-    private javax.swing.JMenuItem forumItem1;
+    private javax.swing.JMenu formulasMenu;
     private javax.swing.JMenuItem forumItem2;
-    private javax.swing.JMenu helpMenu;
-    private javax.swing.JMenu helpMenu1;
     private javax.swing.JMenu helpMenu2;
     private javax.swing.JMenuItem howToItem;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JPopupMenu.Separator jSeparator1;
     private javax.swing.JPopupMenu.Separator jSeparator12;
     private javax.swing.JPopupMenu.Separator jSeparator16;
-    private javax.swing.JMenu matrixMenu;
+    private javax.swing.JPopupMenu.Separator jSeparator2;
+    private javax.swing.JPopupMenu.Separator jSeparator3;
     private javax.swing.JMenuItem onlineHelpItem;
     private javax.swing.JMenuItem printItem;
+    private javax.swing.JMenu rearrageMtxMenu;
     private javax.swing.JMenuItem refreshItem;
     private javax.swing.JMenuItem reportBugItem1;
+    private javax.swing.JMenu specialFunctionsMenu;
     private org.domainmath.gui.StatusPanel statusPanel1;
     private javax.swing.JMenuItem suggestionsItem;
+    private javax.swing.JMenu sumProdMenu;
+    private javax.swing.JMenu trigonometryMenu;
+    private javax.swing.JMenu utilityMenu;
     // End of variables declaration//GEN-END:variables
 }
