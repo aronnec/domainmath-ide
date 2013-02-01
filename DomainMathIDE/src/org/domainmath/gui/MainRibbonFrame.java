6f5185204e169b8182c40c7d86e60ef907ac133f
vinukn <vinuknmgc@gmail.com>
2013-02-01 11:44:46 +0530

    Removed cut,copy paste task bar buttons

--- DomainMathIDE/src/org/domainmath/gui/MainRibbonFrame.java
+++ DomainMathIDE/src/org/domainmath/gui/MainRibbonFrame.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2011 Vinu K.N
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package org.domainmath.gui;
 
@@ -269,14 +285,7 @@ public class MainRibbonFrame extends JRibbonFrame {
         this.getRibbon().addTaskbarComponent(this.undoTaskBarButton);
         redoTaskBarButton = new JCommandButton(null,getResizableIcoFromResource24("resources/icons/size24/edit-redo.png"));
         this.getRibbon().addTaskbarComponent(this.redoTaskBarButton);
-        cutTaskBarButton = new JCommandButton(null,getResizableIcoFromResource24("resources/icons/size24/edit-cut.png"));
-        this.getRibbon().addTaskbarComponent(this.cutTaskBarButton);
-        copyTaskBarButton = new JCommandButton(null,getResizableIcoFromResource24("resources/icons/size24/edit-copy.png"));
-        this.getRibbon().addTaskbarComponent(this.copyTaskBarButton);
-        pasteTaskBarButton = new JCommandButton(null,getResizableIcoFromResource24("resources/icons/size24/edit-paste.png"));
-        this.getRibbon().addTaskbarComponent(this.pasteTaskBarButton);
-
-        
+  
     }
     private void initComponents() {
        ribbonApplicationMenu = new RibbonApplicationMenu();
