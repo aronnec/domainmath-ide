707cddb0f11a72adc4545c6b35abd3290cdabdd2
vinukn <vinuknmgc@gmail.com>
2013-03-06 17:24:26 +0530

    Optimization Tool finished

--- DomainMathIDE/src/org/domainmath/gui/packages/optim/GlpkPanel.java
+++ DomainMathIDE/src/org/domainmath/gui/packages/optim/GlpkPanel.java
@@ -578,7 +578,7 @@ public class GlpkPanel extends javax.swing.JPanel {
             }else{
                 declare("param.lpsolver","2");
             }
-            MainFrame.octavePanel.eval("[xopt, fmin, status, extra] = glpk (c, A, b, lb, ub, ctype, vartype, s, param)");
+            MainFrame.octavePanel.eval("[xopt, fmin, status, extra] = glpk (c, A, b, lb, ub, ctype, vartype, sense, param)");
             
 
              MainFrame.octavePanel.evaluate(jar_path);
